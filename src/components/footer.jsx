491a4d9e62888838d34d8ab5e25243e8d664e986
Wide97 <mwidesott@gmail.com>
2024-08-30 11:34:38 +0200

    fissato in fondo alla pagina footer

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Container, Row, Col } from 'react-bootstrap';
 
 const Footer = () => (
-  <footer className="bg-dark text-light py-3">
+  <footer className="bg-dark text-light py-3 fixed-bottom">
     <Container>
       <Row>
         <Col className="text-center">
