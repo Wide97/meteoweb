f1c87538c998ef7d8337a4d4072f0ef5a56441c0
Wide97 <mwidesott@gmail.com>
2024-08-30 12:36:38 +0200

    text- center

--- src/components/weatherCard.jsx
+++ src/components/weatherCard.jsx
@@ -1,6 +1,6 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import { Card } from 'react-bootstrap';
+import React from "react";
+import { Link } from "react-router-dom";
+import { Card } from "react-bootstrap";
 
 const WEATHER_ICON_URL = "http://openweathermap.org/img/wn/";
 
@@ -10,18 +10,33 @@ const WeatherCard = ({ city }) => {
   }
 
   const { id, name, main, weather } = city;
-  const weatherDescription = weather[0]?.description || 'No description available';
-  const weatherIcon = weather[0]?.icon || '01d'; 
+  const weatherDescription =
+    weather[0]?.description || "No description available";
+  const weatherIcon = weather[0]?.icon || "01d";
 
   return (
     <Card className="weather-card">
       <Card.Body>
         <Card.Title>{name}</Card.Title>
-        <Card.Img variant="top" src={`${WEATHER_ICON_URL}${weatherIcon}@2x.png`} alt={weatherDescription} />
-        <Card.Text>Temperature: {main.temp}°C</Card.Text>
-        <Card.Text>Weather: {weatherDescription}</Card.Text>
-        <Card.Text>Humidity: {main.humidity}%</Card.Text>
-        <Link to={`/city/${id}`} className="btn btn-primary">View Details</Link>
+        <Card.Img
+          variant="top"
+          src={`${WEATHER_ICON_URL}${weatherIcon}@2x.png`}
+          alt={weatherDescription}
+        />
+        <Card.Text className="text-center">
+          Temperature: {main.temp}°C
+        </Card.Text>
+        <Card.Text className="text-center">
+          Weather: {weatherDescription}
+        </Card.Text>
+        <Card.Text className="text-center">
+          Humidity: {main.humidity}%
+        </Card.Text>
+        <div className="text-center">
+          <Link to={`/city/${id}`} className="btn btn-primary">
+            View Details
+          </Link>
+        </div>
       </Card.Body>
     </Card>
   );
